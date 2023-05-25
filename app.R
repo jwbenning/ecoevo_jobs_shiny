e03bf2983eb7359d6909ddc3803191e79e8b54d0
jwbenning <jwbenning@gmail.com>
2023-05-25 12:35:11 -0500

    fixed a couple bugs with factor ordering, and legend display

--- app.R
+++ app.R
@@ -11,8 +11,8 @@ ui <- fluidPage(
     column(width = 4,
            div(style = "padding: 10px; background-color: #f7f7f7;",
              helpText("Select the variables to plot against one another:"),
-             selectInput("x_axis", "X-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Publication Rate"),
-             selectInput("y_axis", "Y-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Offer Rate"),
+             selectInput("x_axis", "X-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "First Author Publication Rate", "Senior Author Pubs", "Total Publications", "Overall Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Overall Publication Rate"),
+             selectInput("y_axis", "Y-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "First Author Publication Rate", "Senior Author Pubs", "Total Publications", "Overall Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Offer Rate"),
              selectInput("color_var", "Color by:", choices = c("None", "Job Cycle", "Current Position", "Gender"), selected = "None"),
              checkboxInput("postdocOnly", "Postdoc data only", value = FALSE),
              checkboxInput("noCB", "No confidence bands", value = FALSE)
@@ -34,7 +34,7 @@ ui <- fluidPage(
       <p>Point ranges for categorical data show mean and 95% confidence limits estimated from bootstrapping (mean_cl_boot function)</p>
       <p>Download the original, cleaned, and R-processed data used in this app <a href='https://docs.google.com/spreadsheets/d/1Wmz6N01mSrpcTfIoMH0Rg42xL64SRKYKzIrAZmDGQ7U/edit#gid=697264455' target='_blank'>here</a>. Data cleaning was necessarily subjective to some extent. All data cleaning steps are recorded in `Data Cleaning` fields for individual records, and on the 'General Cleaning Notes' worksheet. App data will be updated semi-regularly to include new records added via EcoEvoJobs.</p>
       <p>Interpret all relationships with a grain of salt -- who knows if the sampling here is representative, and there is inherent messiness (e.g., 'Offers' could encompass R1, SLAC, and even non-academic job offers). </p>
-      <p>Email comments/bugs to jwbenning@gmail.com</p>")
+      <p>Email comments / bugs to jwbenning@gmail.com</p>")
   )
 )
 
@@ -77,7 +77,8 @@ sheet_data <- read_csv(csv_export_url, na = c("", "NULL", "NA")) %>%
   mutate(`Phone Interview Rate` = `Phone Interviews` / Applications,
          `Campus Interview Rate` = `Campus Interviews` / Applications,
          `Offer Rate` = Offers / Applications,
-         `Publication Rate` = `Total Publications` / `Years Since PhD`,
+         `Overall Publication Rate` = `Total Publications` / `Years Since PhD`,
+         `First Author Publication Rate` = `First Author Pubs` / `Years Since PhD`,
          `Current Position` = fct_relevel(`Current Position`, "Grad Student", "Postdoc", "Non TT faculty", "Asst Prof", "Other"),
          `Total Funding` = fct_relevel(as.factor(`Total Funding`), "$1-50,000", "$50,000-100,000", "$100,000-250,000", "$250,000-500,000", "$500,000-$1M", "$1-2M", "$2M+"))
 
@@ -90,6 +91,10 @@ server <- function(input, output) {
     
     if(is.numeric(sheet_data[[input$y_axis]])) {
       filtered_data <- filtered_data[is.finite(filtered_data[[input$y_axis]]), ]
+    }
+    
+    if(is.numeric(sheet_data[[input$x_axis]])) {
+      filtered_data <- filtered_data[is.finite(filtered_data[[input$x_axis]]), ]
     }
     
     if (input$x_axis == "Total Funding") {
@@ -139,6 +144,7 @@ server <- function(input, output) {
               axis.text.x = element_text(size = 14, angle = 45, vjust = 0.6),
               legend.text = element_text(size = 12),
               legend.position = "bottom") +
+        guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
         labs(fill = input$y_axis) +
         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
       
@@ -165,6 +171,7 @@ server <- function(input, output) {
               axis.text.x = element_text(size = 14, angle = 45, vjust = 0.6),
               legend.text = element_text(size = 12),
               legend.position = "bottom") +
+        guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
       
       print(p)
@@ -182,82 +189,13 @@ server <- function(input, output) {
               axis.text = element_text(size = 14),
               legend.text = element_text(size = 12),
               legend.position = "bottom") +
+        guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
       
       print(p)
     }
   })
 }
-
-
-
-
-
-# server <- function(input, output) {
-#   data <- reactive({
-#     filtered_data <- sheet_data[!is.na(sheet_data[[input$y_axis]]) & is.finite(sheet_data[[input$y_axis]]), ]
-#     
-#     if (input$x_axis == "Total Funding") {
-#       filtered_data <- filtered_data[!is.na(filtered_data$`Total Funding`), ]
-#     }
-#     
-#     if (input$postdocOnly) {
-#       filtered_data <- filtered_data[filtered_data$`Current Position` == "Postdoc", ]
-#     }
-#     
-#     return(filtered_data)
-#   })
-#   
-#   output$scatter_plot <- renderPlot({
-#     plot_data <- data()
-#     
-#     if (input$color_var != "None") {
-#       color_aes <- aes(color = !!sym(input$color_var))
-#     } else {
-#       color_aes <- aes()
-#     }
-#     
-#     if (input$x_axis %in% categorical_vars) {
-#       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
-#         color_aes +
-#         geom_jitter(width = 0.3, height = 0, alpha = 0.5)
-#       
-#       if(input$color_var != "None") {
-#         p <- p + stat_summary(fun.data = "mean_cl_boot", geom = "pointrange", width = 0.2, size = 1, position = position_dodge(0.2))
-#       } else {
-#         p <- p + stat_summary(fun.data = "mean_cl_boot", geom = "pointrange", width = 0.2, size = 1) +
-#           stat_summary(fun.y = mean, geom = "text", aes(label = round(..y.., 2)), vjust = -1, size = 6, position = position_nudge(x = -0.2))
-#       }
-#       
-#       p <- p + labs(x = input$x_axis, y = input$y_axis) +
-#         theme_minimal() +
-#         theme(axis.title = element_text(size = 18),
-#               axis.text.y = element_text(size = 14),
-#               axis.text.x = element_text(size = 14, angle = 45, vjust = 0.6),
-#               legend.position = "bottom") +
-#         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
-#       
-#       print(p)
-#       
-#     } else {
-#       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
-#         color_aes +
-#         geom_smooth(method = "loess", alpha = 0.1, se = !input$noCB) +  # Use input$noCB to toggle confidence bands
-#         geom_point(alpha = 0.5) +
-#         labs(x = input$x_axis, y = input$y_axis) +
-#         theme_minimal() +
-#         theme(axis.title = element_text(size = 18),
-#               axis.text = element_text(size = 14),
-#               legend.position = "bottom") +
-#         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
-#       
-#       print(p)
-#     }
-#   })
-# }
-
-
-
 
 
 
