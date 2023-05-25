a3abbc96225b05beff56e8ff93cd2786da603d43
jwbenning <jwbenning@gmail.com>
2023-05-25 11:05:21 -0500

    updated

--- app.R
+++ app.R
@@ -5,33 +5,43 @@ library(tidyverse)
 library(Hmisc)
 
 ui <- fluidPage(
-  titlePanel("EEB Academic Job Market - 2021/2022 and 2022/2023"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Select the variables to plot against one another:"),
-      selectInput("x_axis", "X-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Offers", "Offer Rate"), selected = "Total Publications"),
-      selectInput("y_axis", "Y-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Offers", "Offer Rate"), selected = "Offer Rate"),
-      selectInput("color_var", "Color by:", choices = c("None", "Job Cycle", "Current Position", "Gender"), selected = "None"),
-      checkboxInput("postdocOnly", "Postdoc data only", value = FALSE),
-      checkboxInput("noCB", "No confidence bands", value = FALSE),
-      HTML("<p>Data from <a href='https://docs.google.com/spreadsheets/d/1cqTuSeLtH-Zw7X9ZtnhQxzw3r19Rya9nzdqRW9apTmY/edit#gid=1335543736' target='_blank'>ecoevojobs</a>. Add your data if you were on the job market this year!</p>
-           <p>'Offer Rate' = Offers / Applications</p>
-           <p>'Publication Rate' = Total Publications / Years Since PhD</p>
-           <p>'Classes taught' refers to the number of times an applicant was an Instructor of Record</p>
-           <p>Loess regression lines have 95% confidence interval bands</p>
-           <p>Point ranges for categorical data show mean and 95% confidence limits estimated from bootstrapping (mean_cl_boot function)</p>
-           <p>Records are removed during data cleaning steps if they contain invalid data types, which is why sample size here may be different from the ecoevojobs sheet</p>
-           <p>Email comments/bugs to jwbenning@gmail.com</p>") 
+  titlePanel("Explore the EEB Job Market"),
+  h4(HTML("Data from <a href='https://docs.google.com/spreadsheets/d/1cqTuSeLtH-Zw7X9ZtnhQxzw3r19Rya9nzdqRW9apTmY/edit#gid=1335543736' target='_blank'>EcoEvoJobs.net</a>, for 2021/2022 and 2022/2023 job cycles")),
+  fluidRow(
+    column(width = 4,
+           div(style = "padding: 10px; background-color: #f7f7f7;",
+             helpText("Select the variables to plot against one another:"),
+             selectInput("x_axis", "X-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Publication Rate"),
+             selectInput("y_axis", "Y-axis:", choices = c("Current Position", "Years Since PhD", "Gender", "First Author Pubs", "Senior Author Pubs", "Total Publications", "Publication Rate", "Fellowships", "Major Grants", "Total Funding", "Classes Taught",  "Applications", "Phone Interviews", "Phone Interview Rate", "Campus Interviews", "Campus Interview Rate", "Offers", "Offer Rate"), selected = "Offer Rate"),
+             selectInput("color_var", "Color by:", choices = c("None", "Job Cycle", "Current Position", "Gender"), selected = "None"),
+             checkboxInput("postdocOnly", "Postdoc data only", value = FALSE),
+             checkboxInput("noCB", "No confidence bands", value = FALSE)
+           )
     ),
-    mainPanel(
-      plotOutput("scatter_plot", height = "400px", width = "400px")
+    column(width = 8,
+           mainPanel(
+             plotOutput("scatter_plot", height = "400px", width = "500px")
+           )
     )
+  ),
+  div(
+    style = "padding:10px; background-color: #f7f7f7;",
+    HTML("<p>Add your data <a href='https://forms.gle/Q7DDngHaVGQDiEZc7' target='_blank'>here</a> if you were on the job market this year!</p>
+      <p>'Offer / Interview Rate' = (Offers or Interviews) / Applications</p>
+      <p>'Publication Rate' = Total Publications / Years Since PhD</p>
+      <p>'Classes taught' refers to the number of times an applicant was an Instructor of Record</p>
+      <p>Loess regression lines have 95% confidence interval bands</p>
+      <p>Point ranges for categorical data show mean and 95% confidence limits estimated from bootstrapping (mean_cl_boot function)</p>
+      <p>Download the original, cleaned, and R-processed data used in this app <a href='https://docs.google.com/spreadsheets/d/1Wmz6N01mSrpcTfIoMH0Rg42xL64SRKYKzIrAZmDGQ7U/edit#gid=697264455' target='_blank'>here</a>. Data cleaning was necessarily subjective to some extent. All data cleaning steps are recorded in `Data Cleaning` fields for individual records, and on the 'General Cleaning Notes' worksheet. App data will be updated semi-regularly to include new records added via EcoEvoJobs.</p>
+      <p>Interpret all relationships with a grain of salt -- who knows if the sampling here is representative, and there is inherent messiness (e.g., 'Offers' could encompass R1, SLAC, and even non-academic job offers). </p>
+      <p>Email comments/bugs to jwbenning@gmail.com</p>")
   )
 )
 
+
 #Sys.setenv(VROOM_CONNECTION_SIZE = "462144")
 
-csv_export_url <- "https://docs.google.com/spreadsheets/d/1Wmz6N01mSrpcTfIoMH0Rg42xL64SRKYKzIrAZmDGQ7U/gviz/tq?tqx=out:csv&sheet=Main"
+csv_export_url <- "https://docs.google.com/spreadsheets/d/1Wmz6N01mSrpcTfIoMH0Rg42xL64SRKYKzIrAZmDGQ7U/gviz/tq?tqx=out:csv&sheet=Clean"
 
 sheet_data <- read_csv(csv_export_url, na = c("", "NULL", "NA")) %>%
 #  slice(-2) %>%
@@ -64,16 +74,23 @@ sheet_data <- read_csv(csv_export_url, na = c("", "NULL", "NA")) %>%
     #Offers = as.numeric(Offers), 
     `Total Publications` = `First Author Pubs` + `Senior Author Pubs` + `Other Pubs`
   ) %>%
-  mutate(`Offer Rate` = Offers / Applications,
+  mutate(`Phone Interview Rate` = `Phone Interviews` / Applications,
+         `Campus Interview Rate` = `Campus Interviews` / Applications,
+         `Offer Rate` = Offers / Applications,
          `Publication Rate` = `Total Publications` / `Years Since PhD`,
          `Current Position` = fct_relevel(`Current Position`, "Grad Student", "Postdoc", "Non TT faculty", "Asst Prof", "Other"),
          `Total Funding` = fct_relevel(as.factor(`Total Funding`), "$1-50,000", "$50,000-100,000", "$100,000-250,000", "$250,000-500,000", "$500,000-$1M", "$1-2M", "$2M+"))
 
+categorical_vars <- c("Gender", "Current Position", "Total Funding")
 
 
 server <- function(input, output) {
   data <- reactive({
-    filtered_data <- sheet_data[!is.na(sheet_data[[input$y_axis]]) & is.finite(sheet_data[[input$y_axis]]), ]
+    filtered_data <- sheet_data[!is.na(sheet_data[[input$y_axis]]), ]
+    
+    if(is.numeric(sheet_data[[input$y_axis]])) {
+      filtered_data <- filtered_data[is.finite(filtered_data[[input$y_axis]]), ]
+    }
     
     if (input$x_axis == "Total Funding") {
       filtered_data <- filtered_data[!is.na(filtered_data$`Total Funding`), ]
@@ -86,6 +103,7 @@ server <- function(input, output) {
     return(filtered_data)
   })
   
+  
   output$scatter_plot <- renderPlot({
     plot_data <- data()
     
@@ -95,7 +113,40 @@ server <- function(input, output) {
       color_aes <- aes()
     }
     
-    if (input$x_axis %in% c("Gender", "Current Position", "Total Funding")) {
+    if (input$x_axis %in% categorical_vars && input$y_axis %in% categorical_vars && input$x_axis != input$y_axis) {
+      # Calculate percentage of each group on the x axis belonging to each group on the y axis
+      # percentages <- plot_data %>%
+      #   group_by(!!sym(input$x_axis), !!sym(input$y_axis)) %>%
+      #   summarise(n = n()) %>%
+      #   mutate(percentage = n / sum(n)) %>%
+      #   ungroup()
+      
+      # Calculate counts
+      counts <- plot_data %>%
+        group_by(!!sym(input$x_axis), !!sym(input$y_axis)) %>%
+        summarise(n = n()) 
+      
+      #print(head(plot_data))
+  
+      
+      p <- ggplot(counts, aes(x = !!sym(input$x_axis), y = n, fill = !!sym(input$y_axis))) +
+        geom_bar(stat = 'identity', position = 'dodge') +
+        ylab("Count") +
+        #scale_y_continuous(labels = scales::percent) +
+        theme_minimal() +
+        theme(axis.title = element_text(size = 18),
+              axis.text.y = element_text(size = 14),
+              axis.text.x = element_text(size = 14, angle = 45, vjust = 0.6),
+              legend.text = element_text(size = 12),
+              legend.position = "bottom") +
+        labs(fill = input$y_axis) +
+        annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
+      
+      print(p)
+      
+    }
+    
+    else if (input$x_axis %in% categorical_vars) {
       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
         color_aes +
         geom_jitter(width = 0.3, height = 0, alpha = 0.5)
@@ -112,20 +163,24 @@ server <- function(input, output) {
         theme(axis.title = element_text(size = 18),
               axis.text.y = element_text(size = 14),
               axis.text.x = element_text(size = 14, angle = 45, vjust = 0.6),
+              legend.text = element_text(size = 12),
               legend.position = "bottom") +
         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
       
       print(p)
       
-    } else {
+    } 
+    
+    else {
       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
         color_aes +
-        geom_smooth(method = "loess", alpha = 0.1, se = !input$noCB) +  # Use input$noCB to toggle confidence bands
+        geom_smooth(method = "loess", alpha = 0.15, se = !input$noCB) +  # Use input$noCB to toggle confidence bands
         geom_point(alpha = 0.5) +
         labs(x = input$x_axis, y = input$y_axis) +
         theme_minimal() +
         theme(axis.title = element_text(size = 18),
               axis.text = element_text(size = 14),
+              legend.text = element_text(size = 12),
               legend.position = "bottom") +
         annotate("text", x = Inf, y = Inf, label = paste("n =", nrow(plot_data)), hjust = 1, vjust = 1, size = 5)
       
@@ -162,7 +217,7 @@ server <- function(input, output) {
 #       color_aes <- aes()
 #     }
 #     
-#     if (input$x_axis %in% c("Gender", "Current Position", "Total Funding")) {
+#     if (input$x_axis %in% categorical_vars) {
 #       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
 #         color_aes +
 #         geom_jitter(width = 0.3, height = 0, alpha = 0.5)
@@ -187,7 +242,7 @@ server <- function(input, output) {
 #     } else {
 #       p <- ggplot(plot_data, aes(x = !!sym(input$x_axis), y = !!sym(input$y_axis))) +
 #         color_aes +
-#         geom_smooth(method = "loess", alpha = 0.) +
+#         geom_smooth(method = "loess", alpha = 0.1, se = !input$noCB) +  # Use input$noCB to toggle confidence bands
 #         geom_point(alpha = 0.5) +
 #         labs(x = input$x_axis, y = input$y_axis) +
 #         theme_minimal() +
@@ -200,6 +255,12 @@ server <- function(input, output) {
 #     }
 #   })
 # }
+
+
+
+
+
+
 
 
 
